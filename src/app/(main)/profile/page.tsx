4fbccdc5b1007bb465c258eec8d6aec02507d553
kev1nandreas <kevin.andreascn@gmail.com>
2025-06-04 01:18:39 +0700

    feat: profile page (construction)

---
+++ src/app/(main)/profile/page.tsx
@@ -0,0 +1,10 @@
+function History() {
+  return (
+    <div className="flex flex-col w-[80%] h-[calc(100vh-4rem)] p-[2rem] overflow-y-auto">
+      <h1 className="text-3xl font-bold">Profile</h1>
+      <p className="text-gray-500">This page is under construction.</p>
+    </div>
+  );
+}
+
+export default History;
